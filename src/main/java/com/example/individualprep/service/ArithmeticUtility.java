ba85557a484ebafd856067fec74a197b87aef37c
Aldebaran64 <aldebaran26adhitya@gmail.com>
2026-02-12 15:19:22 +0700

    Corrected folder structure

---
+++ src/main/java/com/example/individualprep/service/ArithmeticUtility.java
@@ -0,0 +1,36 @@
+package com.example.individualprep.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ArithmeticUtility {
+    
+    public double add(double o1, double o2) {
+        // TODO: Implement me properly!
+        return 0.0;
+    }
+
+    public double subtract(double o1, double o2) {
+        // TODO: Implement me properly!
+        return 0.0;
+    }
+
+    public double multiply(double o1, double o2) {
+        // TODO: Implement me properly!
+        return 0.0;
+    }
+
+    public double divide(double o1, double o2) {
+        // TODO: Implement me properly!
+        return 0.0;
+    }
+
+    public double exponent(double o1, int n) {
+
+        if ((o1 == 0.0) && (n < 0)) {
+            throw new IllegalArgumentException("Cannot raise zero to a negative power due to division by zero.");
+        }
+
+        return Math.pow(o1, n);
+    }
+}
---
+++ src/main/java/com/example/individualprep/service/VectorUtility.java
@@ -0,0 +1,47 @@
+package com.example.individualprep.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class VectorUtility {
+    
+    public double[] add(double[] v1, double[] v2) {
+
+        if (v1 == null || v2 == null) {
+            throw new IllegalArgumentException("Vectors must not be null.");
+        }
+
+        if (v1.length != v2.length) {
+            throw new IllegalArgumentException("Vectors must have the same size.");
+        }
+
+        int vectorLength = v1.length;
+        double[] vectorAddResult = new double[vectorLength];
+
+        for(int i = 0; i < vectorLength; i++) {
+            vectorAddResult[i] = v1[i] + v2[i];
+        }
+
+        return vectorAddResult;
+    }
+
+    public double[] subtract(double[] v1, double[] v2) {
+        // TODO: Implement me properly!
+        return new double[] { 0.0, 0.0, 0.0 };
+    }
+
+    public double[] multiply(double[] v1, int x) {
+        // TODO: Implement me properly!
+        return new double[] { 0.0, 0.0, 0.0 };
+    }
+    
+    public double dotProduct(double[] v1, double[] v2) {
+        // TODO: Implement me properly!
+        return 0.0;
+    }
+    
+    public double norm(double[] v1) {
+        // TODO: Implement me properly!
+        return 0.0;
+    }
+}
