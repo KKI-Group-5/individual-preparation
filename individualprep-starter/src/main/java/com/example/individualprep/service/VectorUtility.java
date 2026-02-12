59d92d151f7d606fe7f965614ada3e10fdd76667
kaykaykaykaykaykay <khayrukartajaya360@gmail.com>
2026-02-13 01:18:55 +0700

    added vector dot product

---
+++ individualprep-starter/src/main/java/com/example/individualprep/service/VectorUtility.java
@@ -0,0 +1,42 @@
+package com.example.individualprep.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class VectorUtility {
+    
+    public double[] add(double[] v1, double[] v2) {
+        // TODO: Implement me properly!
+        return new double[] { 0.0, 0.0, 0.0 };
+    }
+
+    public double[] subtract(double[] v1, double[] v2) {
+        // TODO: Implement me properly!
+        return new double[] { 0.0, 0.0, 0.0 };
+    }
+
+    public double[] multiply(double[] v1, int x) {
+        // TODO: Implement me properly!
+        return new double[] { 0.0, 0.0, 0.0 };
+    }
+
+    public double dotProduct(double[] v1, double[] v2) {
+        if (v1 == null || v2 == null) {
+            throw new IllegalArgumentException("Vectors must not be null");
+        }
+        if (v1.length != v2.length) {
+            throw new IllegalArgumentException("Vectors must have the same length");
+        }
+        double result = 0.0;
+        for (int i = 0; i < v1.length; i++) {
+            result += v1[i] * v2[i];
+        }
+        return result;
+    }
+
+
+    public double norm(double[] v1) {
+        // TODO: Implement me properly!
+        return 0.0;
+    }
+}
