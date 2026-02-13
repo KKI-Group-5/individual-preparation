125e87bc2cdd5a4a74a202cad038744b1df2c1c6
Raditya Kusuma <radityakusuma2017@gmail.com>
2026-02-13 09:33:10 +0700

    unit test

--- src/test/java/com/example/individualprep/service/VectorUtilityTest.java
+++ src/test/java/com/example/individualprep/service/VectorUtilityTest.java
@@ -8,4 +8,22 @@ class VectorUtilityTest {
 
     private final VectorUtility vectorUtility = new VectorUtility();
 
+    @Test
+    void norm_shouldReturnEuclideanNorm() {
+        double[] v = {3.0, 4.0};
+        assertEquals(5.0, vectorUtility.norm(v), 1e-9);
+    }
+
+    @Test
+    void norm_shouldWorkForZeros() {
+        double[] v = {0.0, 0.0, 0.0};
+        assertEquals(0.0, vectorUtility.norm(v), 1e-9);
+    }
+
+    @Test
+    void norm_shouldWorkForNegativeValues() {
+        double[] v = {-3.0, -4.0};
+        assertEquals(5.0, vectorUtility.norm(v), 1e-9);
+    }
+
 }
