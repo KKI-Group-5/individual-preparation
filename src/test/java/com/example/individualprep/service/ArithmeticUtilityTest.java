7f6ab4a30187ebc8d7d5b1fb2b6e531ef90e1540
Raditya Kusuma <radityakusuma2017@gmail.com>
2026-02-13 09:30:44 +0700

    unit test

--- src/test/java/com/example/individualprep/service/ArithmeticUtilityTest.java
+++ src/test/java/com/example/individualprep/service/ArithmeticUtilityTest.java
@@ -8,4 +8,19 @@ class ArithmeticUtilityTest {
 
     private final ArithmeticUtility arithmeticUtility = new ArithmeticUtility();
 
+    @Test
+    void add_shouldReturnSum() {
+        assertEquals(12.0, arithmeticUtility.add(5.0, 7.0), 1e-9);
+    }
+
+    @Test
+    void add_shouldHandleNegativeNumbers() {
+        assertEquals(-2.0, arithmeticUtility.add(-5.0, 3.0), 1e-9);
+    }
+
+    @Test
+    void add_shouldHandleDecimals() {
+        assertEquals(0.3, arithmeticUtility.add(0.1, 0.2), 1e-9); // pakai tolerance
+    }
+
 }
